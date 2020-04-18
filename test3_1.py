fd87f0965e43d0f2101db1b5943ed209d66f9f61
T4m3R <tamer.arsayed@gmail.com>
2020-04-18 12:55:40 +0200

    comit 6

--- test3_1.py
+++ test3_1.py
@@ -1,8 +1,15 @@
+print('Instructions: Enter a number followed by Enter to sum. enter reset to reset calculator \n')
 y=0
 while True: 
     x=input('enter a number :')
+    if x=='reset':
+        y=0.0
+        print('\naccumulative sum=',y,'\n')
+        continue
     try: z=float(x)
     except: 
-        if x=='done': break
+        print('enter a numerical value')
+        continue
     y=z+y
-print('y=',y)
+    print('accumulative sum =',y)
+print('inout over \ntotal =',y)
